7cb3c35291d4632df01148535d2c86e383c90ce3
Ayt1da <61749332+Ayt1da@users.noreply.github.com>
2020-05-24 12:56:05 +0530

    Initial commit

---
+++ precache-manifest.93c95345936ba2e49ef69e509c592dd1.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "04c1c5ff08e01365a5bcd7ddd1cd7b18",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4ade87805e3ecea964d7",
+    "url": "/static/js/2.91048d5c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.91048d5c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d8595222394d7349fe4e",
+    "url": "/static/js/main.4daa25c6.chunk.js"
+  },
+  {
+    "revision": "2cebf7a133097aadc63c",
+    "url": "/static/js/runtime-main.14b288d2.js"
+  }
+]);
